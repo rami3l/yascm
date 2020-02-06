781e22184b4fb45d43a1ac55d8ec94513be88656
Rami3L <rami3l@outlook.com>
2020-02-06 23:04:11 +0800

    Completed ScmPrelude.hs

--- src/ScmPrelude.hs
+++ src/ScmPrelude.hs
@@ -10,11 +10,10 @@ prelude :: Env
 prelude =
     let o = Nothing
         d = Map.fromList
-            [ ("+", makePrim add)
-            , ("-", makePrim sub)
-            , ("*", makePrim mul)
-            , ("/", makePrim ScmPrelude.div)
-            {-
+            [ ("+"   , makePrim add)
+            , ("-"   , makePrim sub)
+            , ("*"   , makePrim mul)
+            , ("/"   , makePrim ScmPrelude.div)
             , ("="   , makePrim eq)
             , ("<"   , makePrim lt)
             , ("<="  , makePrim le)
@@ -23,6 +22,7 @@ prelude =
             , ("car" , makePrim car)
             , ("cdr" , makePrim cdr)
             , ("cons", makePrim cons)
+            , ("list", makePrim list)
             , ( "null?"
               , makePrim isNull
               )
@@ -32,7 +32,6 @@ prelude =
             , ("#t"  , Boolean True)
             , ("#f"  , Boolean False)
             , ("null", List [])
-            -}
             ]
     in  Env d o
 
@@ -60,3 +59,48 @@ div [(Number x), (Number y)] = if y /= 0
     then Right $ Number (x / y)
     else Left $ ScmErr $ "div: divided by 0"
 div _ = Left $ ScmErr $ "div: expected Number"
+
+eq :: [Exp] -> Either ScmErr Exp
+eq [(Number  x), (Number y) ] = Right $ Boolean (x == y)
+eq [(Boolean x), (Boolean y)] = Right $ Boolean (x == y)
+eq _                          = Left $ ScmErr $ "eq: expected Number or Boolean"
+
+lt :: [Exp] -> Either ScmErr Exp
+lt [(Number  x), (Number y) ] = Right $ Boolean (x < y)
+lt [(Boolean x), (Boolean y)] = Right $ Boolean (x < y)
+lt _                          = Left $ ScmErr $ "lt: expected Number or Boolean"
+
+le :: [Exp] -> Either ScmErr Exp
+le [(Number  x), (Number y) ] = Right $ Boolean (x <= y)
+le [(Boolean x), (Boolean y)] = Right $ Boolean (x <= y)
+le _                          = Left $ ScmErr $ "le: expected Number or Boolean"
+
+gt :: [Exp] -> Either ScmErr Exp
+gt [(Number  x), (Number y) ] = Right $ Boolean (x > y)
+gt [(Boolean x), (Boolean y)] = Right $ Boolean (x > y)
+gt _                          = Left $ ScmErr $ "gt: expected Number or Boolean"
+
+ge :: [Exp] -> Either ScmErr Exp
+ge [(Number  x), (Number y) ] = Right $ Boolean (x >= y)
+ge [(Boolean x), (Boolean y)] = Right $ Boolean (x >= y)
+ge _                          = Left $ ScmErr $ "ge: expected Number or Boolean"
+
+car :: [Exp] -> Either ScmErr Exp
+car [List (x : xs)] = Right x
+car _               = Left $ ScmErr $ "car: expected a List"
+
+cdr :: [Exp] -> Either ScmErr Exp
+cdr [List (x : xs)] = Right $ List xs
+cdr _               = Left $ ScmErr $ "cdr: expected a List"
+
+cons :: [Exp] -> Either ScmErr Exp
+cons [x, (List y)] = Right $ List (x : y)
+cons _             = Left $ ScmErr $ "cons: expected an Exp and a List"
+
+list :: [Exp] -> Either ScmErr Exp
+list xs = Right $ List xs
+
+isNull :: [Exp] -> Either ScmErr Exp
+isNull [List []] = Right $ Boolean True
+isNull [List _ ] = Right $ Boolean False
+isNull _         = Left $ ScmErr $ "null?: expected a List"
