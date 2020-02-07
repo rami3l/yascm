48995d9c496f2d25ab78f763e74590a1e45c2f3c
Rami3L <rami3l@outlook.com>
2020-02-07 13:48:16 +0800

    Finished automated test setup

--- src/Repl.hs
+++ src/Repl.hs
@@ -10,6 +10,7 @@ import qualified ScmPrelude                    as Scm
 import           Control.Monad.State
 import           System.Console.Haskeline
 
+repl :: T.Env -> IO ()
 repl env = runInputT defaultSettings (loop env)
   where
     loop :: T.Env -> InputT IO ()
@@ -18,7 +19,7 @@ repl env = runInputT defaultSettings (loop env)
         case mline of
             Nothing   -> loop env
             Just line -> case P.run line of
-                Right exp ->
-                    let (val, env') = runState (E.eval exp) env
+                Right expr ->
+                    let (val, env') = runState (E.eval expr) env
                     in  outputStrLn ("=> " ++ show val) >> loop env'
                 Left e -> outputStrLn (show e) >> loop env
