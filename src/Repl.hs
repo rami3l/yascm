00a3316883d9371c45aadd6bbdbad678b96fca80
rami3l <rami3l@outlook.com>
2020-08-25 15:22:42 +0800

    refactor: improve coding style, take 2

--- src/Repl.hs
+++ src/Repl.hs
@@ -11,6 +11,7 @@ import qualified EvalApply                     as E
 import           Control.Monad.State
 import           System.Console.Haskeline
 import           Data.IORef
+import           Data.Function
 
 repl :: IORef T.Env -> IO ()
 repl envBox = runInputT defaultSettings (loop envBox)
@@ -18,20 +19,18 @@ repl envBox = runInputT defaultSettings (loop envBox)
     loop :: IORef T.Env -> InputT IO ()
     loop envBox' = do
         mLine <- getInputLine ">> "
-        maybe
+        mLine & maybe
             (return ())
-            (\line -> either
+            (\line -> P.run line & either
                 (outputStrLn . show)
                 (\expr -> do
                     mval <- liftIO $ E.eval expr envBox'
                     case mval of
                         Right (T.Empty) -> return ()
-                        Right val       -> outputStrLn ("=> " ++ show val)
-                        Left  e         -> outputStrLn (show e)
+                        Right val       -> outputStrLn $ "=> " ++ show val
+                        Left  e         -> outputStrLn $ show e
                 )
-                (P.run line)
             )
-            mLine
         loop envBox'
 
 -- Get a list of Scheme Expression String's, and return the corresponding output String's
@@ -39,15 +38,14 @@ runStrings :: [String] -> IORef T.Env -> IO [String]
 runStrings xs envBox = foldl seedGen (return []) xs  where
     seedGen mSeed x = do
         seed <- mSeed
-        either
+        P.run x & either
             (\err -> return $ seed ++ [show err])
             (\expr -> do
                 val <- E.eval expr envBox
                 return $ seed ++ [show val]
             )
-            (P.run x)
 
 -- Get a String of Scheme Expression's, then read and eval all of them
 runScheme :: String -> IORef T.Env -> IO (Either T.ScmErr T.Exp)
 runScheme str envBox =
-    either (return . Left) (\xs -> E.evalList xs envBox) $ P.runList str
+    P.runList str & either (return . Left) (\xs -> E.evalList xs envBox)
