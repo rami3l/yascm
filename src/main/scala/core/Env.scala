afdfaccbc2e137f8b9893a3e904de61009101eec
rami3l <rami3l@outlook.com>
2021-11-26 16:56:33 +0100

    refactor: rewrite `Env.lookup` and `Env.setVal`

--- src/main/scala/core/Env.scala
+++ src/main/scala/core/Env.scala
@@ -5,26 +5,17 @@ import scala.compiletime.ops.boolean
 import scala.annotation.tailrec
 
 class Env(val dict: HashMap[String, Exp] = HashMap(), val outer: Option[Env]) {
+  private def lookupWithEnv(sym: String): Option[(Env, Exp)] =
+    dict.get(sym).map { (this, _) }.orElse {
+      outer.flatMap(_.lookupWithEnv(sym))
+    }
 
   /** Find the definition of a symbol.
     */
-  def lookup(sym: String): Option[Exp] =
-    dict.get(sym).orElse { outer.flatMap { _.lookup(sym) } }
+  def lookup(sym: String): Option[Exp] = lookupWithEnv(sym).map(_._2)
 
   def insertVal(sym: String, defn: Exp): Unit = dict += (sym -> defn)
 
-  @tailrec
-  final def setVal(
-      sym: String,
-      defn: Exp,
-      isSymDefined: Boolean = false
-  ): Unit = {
-    lazy val isSymLocal = dict.get(sym).isDefined
-    lazy val isSymDefined1 = isSymDefined || lookup(sym).isDefined
-    if (!isSymLocal && isSymDefined1)
-      // If `sym` is not local but is defined, then `outer` must be defined.
-      outer.get.setVal(sym, defn, true)
-    else
-      insertVal(sym, defn)
-  }
+  def setVal(sym: String, defn: Exp): Unit =
+    lookupWithEnv(sym).map(_._1).getOrElse(this).insertVal(sym, defn)
 }
